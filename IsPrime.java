52b2d461c6d3a8a4f45747baadc5878921959154
boslanger <43486210+boslanger@users.noreply.github.com>
2019-05-02 21:05:56 -0500

    Update IsPrime.java

--- IsPrime.java
+++ IsPrime.java
@@ -3,39 +3,44 @@
  * Coding challenge - Is the number prime?
  *  TODO: 
  *  - add a input error statement block.
- *  - verify that the loop stops when i > userRequest.
  */
 
-import java.util.Scanner;
+import java.time.Instant;
+// import java.util.Scanner;
 
-public class IsPrime {
+
+/* takes a user supplied integer and determines all divisors and if it is prime.
+ * int 100,000 time tested at 25-30 milliseconds.
+ * int 1,000,000 time tested at 30-35 milliseconds.
+ * int 1,000,000,000 time tested at 3200 - 3220 milliseconds. 
+ */
+public class isPrime {
 	
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		boolean prime = true; // changed when loop finds a divisor. 
-		
+		long startTime = Instant.now().toEpochMilli(); // start timing
+//		Scanner in = new Scanner(System.in);
+		 
 		System.out.println("Check to see if a number is prime.");
-		int userRequest = in.nextInt();	
+//		int userRequest = in.nextInt();	
+		int userRequest = 1_000_000_000;	
 		
-		/* build the array. */
-		int checkArray[] = new int [userRequest-2]; 
-		for(int i = 0; i < userRequest-2; ++i) { // avoids the zero error.
-			checkArray[i] = i+2; // avoids errors
-		}
-		
-		/* loop to control the checking */
-		for(int i = 0; i < checkArray.length; ++i) {
-			if(userRequest % checkArray[i] == 0 && userRequest != checkArray[i] ) {
-				System.out.println(userRequest + " is divisable by " + checkArray[i] + ".");
+		boolean prime = true;
+		/* I found this loop to be much faster at higher numbers */
+		for(int i = 2; i < userRequest; ++i) { // start on 2 because 1 divides into everything.
+			if(userRequest % i == 0) {
 				prime = false;
+				System.out.println(userRequest + " is divisable by " + i + ".");
 			}
+			if(prime) {
+				System.out.println(userRequest + " is prime.");
+			}	
 		}
-		
-		if(prime) {
-			System.out.println(userRequest + " is prime.");
-		}
-		
+
+		long endTime = Instant.now().toEpochMilli(); // stop timing
+		long timeElapsed = endTime - startTime; 
 		in.close(); // closes the scanner.
+		
+		System.out.println(timeElapsed + " milliseconds"); // displays the speed of execution. 
 		System.out.println("terminated.");
 	}
 }
